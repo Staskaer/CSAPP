e9f603ca84789e98ac00acc7579c19c5eeb6f2af
ppxxxg22 <liujiaxin011121@gmail.com>
2022-07-15 18:25:56 +0800

    learning_sum

---
+++ chapter 7/learning/sum/main.s
@@ -0,0 +1,33 @@
+	.file	"main.c"
+	.text
+	.globl	array
+	.data
+	.align 8
+array:
+	.long	1
+	.long	2
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$2, %edx
+	leaq	array(%rip), %rcx
+	call	sum
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	sum;	.scl	2;	.type	32;	.endef
---
+++ chapter 7/learning/sum/sum.s
@@ -0,0 +1,37 @@
+	.file	"sum.c"
+	.text
+	.globl	sum
+	.def	sum;	.scl	2;	.type	32;	.endef
+	.seh_proc	sum
+sum:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$16, %rsp
+	.seh_stackalloc	16
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	$0, -8(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	addl	%eax, -8(%rbp)
+	addl	$1, -4(%rbp)
+.L2:
+	movl	-4(%rbp), %eax
+	cmpl	24(%rbp), %eax
+	jl	.L3
+	movl	-8(%rbp), %eax
+	addq	$16, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
