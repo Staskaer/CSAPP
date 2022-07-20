2ff558ccd7e255e2996ec8227f6caa04f9af234a
ppxxxg22 <liujiaxin011121@gmail.com>
2022-07-20 20:47:10 +0800

    dll

---
+++ chapter 7/learning/dll/addvec.c
@@ -0,0 +1,14 @@
+int addcnt = 0;
+
+void addvec(int *x, int *y, int *z, int n)
+{
+    int i;
+    addcnt++;
+
+    int xy = 100;
+
+    for (i = 0; i < n; i++)
+    {
+        z[i] = x[i] + y[i];
+    }
+}
